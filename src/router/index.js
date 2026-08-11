d28c7a449d2803834e72254efb15bde318af5070
JackeySkywave <108508286+jackeyskywave@users.noreply.github.com>
2026-08-11 12:46:35 +0530

    Remove hash # from URL using HTML5 history mode and SPA fallback

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { authService } from '../services/authService';
 
 const routes = [
@@ -27,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory('/society-management/'),
   routes
 });
 
